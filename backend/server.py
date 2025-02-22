129e1c6aff535c1e9de4045dfcfd6f1897e17fc8
Pranay Nandkeolyar <pranay.nandkeolyar@gmail.com>
2025-02-22 16:23:04 -0500

    cleaned backend and working on nav

--- backend/server.py
+++ backend/server.py
@@ -88,29 +88,6 @@ def signup_handler():
 
 
 
-@app.route('/add_member', methods=["GET", "POST"])  # Ensure POST is allowed
-def add_member():
-    # Get the JSON data from the request
-    data = request.get_json()
-    print(data)
-    # Make sure the data includes a 'name' field
-    if 'name' not in data:
-        return jsonify(message="Error: 'name' is required"), 400
-
-    # Insert the new member into the 'members' collection
-    members_collection = db.members
-    member = {"name": data["name"]}
-    members_collection.insert_one(member)
-    member["_id"] = str(member["_id"])  # Convert ObjectId to string
-    return jsonify(message="Member added successfully!", member=member), 201
-
-# Fetch members from MongoDB
-@app.route("/members")
-def members():
-    members_collection = db.members
-    members = [member["name"] for member in members_collection.find()]
-    return jsonify({"members": members})
-
 if __name__ == "__main__":
     try:
         client.admin.command('ping')  # Check MongoDB connection
