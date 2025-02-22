bbae285709b6f9db8f8da3e9bf8c120836dfee3c
anish-mangla <mangla0@purdue.edu>
2025-02-22 03:21:23 -0500

    updated the navbar

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import logo from "./assets/logo.png";
 
+// Uncomment and adjust this if you have a logo file 
+// import logo from "../assets/logo.png";
 
 const Navbar = () => {
   return (
-    // Using a semantic <nav> for the navbar container
     <nav className="navbar-container">
+      {/* Logo section on the left */}
+      <div className="navbar-logo">
+        {/* If you have a logo file, use <img src={logo} alt="Logo" /> */}
+        {/* or you can simply use text if you prefer */}
+        <Link to="/" className="logo-link">
+          {/*  */}
+          <img src={logo} alt="Logo" className="logo-img" />
+        </Link>
+      </div>
 
-      {/* Link section */}
+      {/* Link section on the right */}
       <div className="navbar-links">
         <Link to="/" className="nav-link">
           Home
